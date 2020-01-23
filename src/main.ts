dd11956840d0d1fa1e8275c4564818a1cccc978e
Koushik Kintali <koushik.kintali@gmail.com>
2020-01-23 11:11:29 +0530

    remove lint errors

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
-import { document, field } from "./decorators";
+import { document, field } from './decorators';
 
 @document
 class Person {
-    @field("string", true)
-    fistname: string;
-    
-    @field("string", true)
-    lastname: string;
-    
-    constructor(firstname: string, lastname: string) {
-        this.fistname = firstname;
-        this.lastname = lastname;
-    }
-    getFullName() {
-        return `${this.fistname} ${this.lastname}`;
-    }
+  @field('string', true)
+  firstname: string;
+
+  @field('string', true)
+  lastname: string;
+
+  constructor(firstname: string, lastname: string) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+  getFullName() {
+    return `${this.firstname} ${this.lastname}`;
+  }
 }
 
-const p = new Person("A", "B");
-const p2 = new Person("C", "D");
+const p = new Person('A', 'B');
+const p2 = new Person('C', 'D');
