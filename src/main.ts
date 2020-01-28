d5d77540a0516af9b7f75a62185fe90b7801a5f1
satyendraXV <satyendra.singh@talentica.com>
2020-01-28 06:52:00 +0000

    create collection

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { defaultMetadataStorage } from './metadata/metadata.storage';
 import { FieldMetadata } from './metadata/field.metadata';
+import * as mongoose from 'mongoose';
 export * from './decorators/document/document.decorator';
 export * from './decorators/field/field.decorator';
 
-export function createCollection(mongoose: any, documents: Function[]) {
+// TODO: take as config
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connected...!');
+});
+
+export function createCollection(documents: Function[]) {
     if (documents && documents.length) {
         documents.forEach((document) => {
             const [collectionMetadata] = defaultMetadataStorage.findCollectionMetadatasForClass(document);
@@ -13,13 +23,11 @@ export function createCollection(mongoose: any, documents: Function[]) {
             fieldMetadatas.forEach((fieldMetadata: FieldMetadata) => {
                 fieldData[fieldMetadata.propertyName] = fieldMetadata.type;
             });
-
-            console.log(fieldData);
             const schema = new mongoose.Schema(fieldData);
-            console.log(schema);
             const model = mongoose.model(collectionMetadata.name, schema);
-            console.log(model);
+            model.createCollection().then((collection: any) => {
+                console.log('Created collection: ', collection.name);
+            });
         });
-        console.log('Created...!');
     }
 }
--- test/src/main.ts
+++ test/src/main.ts
@@ -1,18 +1,4 @@
-import * as mongoose from 'mongoose';
 import { createCollection } from 'orm';
 import { Person } from './models/person.model';
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    // createCollection(mongoose, [Person]);
-
-    const schema = new mongoose.Schema({
-        name: String,
-    });
-    const model = mongoose.model('cat', schema);
-
-    console.log('--------');
-});
+createCollection([Person]);
