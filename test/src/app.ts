4cea91bc5a4ab8f1146b9edc45b5ef960b5cead4
Ajeet Shah <ajeets.shah@hotmail.com>
2020-02-09 17:27:26 +0530

    Add express in test

--- test/src/app.ts
+++ test/src/app.ts
@@ -1,39 +1,18 @@
-import * as http from 'http'
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import employeeRouter from './routes/employee.routes'
 import { createConnection } from 'orm'
-import { employeeController } from './controllers/employee.controller'
 
 createConnection('mongodb://localhost:27017/orm-test')
 
-const port = 8081
+const app = express()
+const port = 3000
 
-http.createServer(function (req, res) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
-    function end(data, statusCode = 200, header?: http.OutgoingHttpHeaders) {
-        header = header ? header : { 'Content-Type': 'application/json' }
-        res.writeHead(statusCode, header);
-        res.write(JSON.stringify(data))
-        res.end()
-    }
+app.use('/employees', employeeRouter);
 
-    let data: any = []
-    req.on('data', chunk => {
-        data.push(chunk)
-    })
-
-    req.on('end', () => {
-        data = (data.length && JSON.parse(data)) || {}
-        const url = req.url
-        switch (url.toLowerCase()) {
-            case '/emp/create':
-            case '/emp/fetch':
-                employeeController(url, data).then(result => {
-                    end(result, 200)
-                }).catch(err => {
-                    end(err, 400)
-                })
-        }
-    })
-
-}).listen(port, () => {
-    console.log(`Started server at post ${port}`)
-});
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}!`)
+})
