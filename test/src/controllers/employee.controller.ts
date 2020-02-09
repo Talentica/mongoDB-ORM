1ac9871b89d8225672bc17302fd6d5fc30243a77
Ajeet Shah <ajeetshah@users.noreply.github.com>
2020-02-09 17:28:52 +0530

    Add express in test (#6)

--- test/src/controllers/employee.controller.ts
+++ test/src/controllers/employee.controller.ts
@@ -1,38 +1,26 @@
 import { Employee } from '../models/employee.model'
+import * as express from 'express'
 
 const EmployeeModel = (new Employee() as any).getModel()
 
-export function employeeController(path: string, data: any): Promise<any> {
-    switch (path) {
-        case '/emp/create':
-            return create(data)
-        case '/emp/fetch':
-            return fetch()
-    }
-}
-
-function create(data: Employee): Promise<any> {
-    return new Promise((resolve, reject) => {
-        const emp: any = new EmployeeModel()
-        emp.id = data.id
-        emp.name = data.name
-        emp.save((err, result) => {
-            if (err) {
-                reject(err)
-            }
-            resolve(result)
-        })
+export function create(req: express.Request, res: express.Response) {
+    const data = req.body
+    const emp: any = new EmployeeModel()
+    emp.id = data.id
+    emp.name = data.name
+    emp.save((err, result) => {
+        if (err) {
+            res.send(err)
+        }
+        res.send(result)
     })
 }
 
-function fetch(): Promise<any> {
-    return new Promise((resolve, reject) => {
-        EmployeeModel.find((err, result) => {
-            if (err) {
-                reject(err)
-            }
-            resolve(result)
-        })
+export function fetch(req, res) {
+    EmployeeModel.find((err, result) => {
+        if (err) {
+            res.send(err)
+        }
+        res.send(result)
     })
-
-}
+};
