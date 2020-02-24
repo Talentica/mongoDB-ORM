bb4d3a88467e4f48d91d34d25f67665b06710a29
Ajeet Shah <ajeets.shah@hotmail.com>
2020-02-24 23:39:29 +0530

    connection strings

--- test/src/app.ts
+++ test/src/app.ts
@@ -5,8 +5,9 @@ import teacherRoutes from './routes/teacher.routes';
 import studentRoutes from './routes/student.routes';
 import { Teacher } from './models/teacher.model';
 
-createConnection('mongodb+srv://satyendra-singh-talentica:@nonym0us@cluster0-3lp3s.mongodb.net/test?retryWrites=true&w=majority');
-createConnection('mongodb+srv://koushik:koushik@cluster0-br4jq.mongodb.net/test?retryWrites=true&w=majority');
+// createConnection('mongodb+srv://satyendra-singh-talentica:@nonym0us@cluster0-3lp3s.mongodb.net/test?retryWrites=true&w=majority');
+// createConnection('mongodb+srv://koushik:koushik@cluster0-br4jq.mongodb.net/test?retryWrites=true&w=majority');
+createConnection('mongodb://localhost:27017/orm-test');
 
 // const app = express();
 // const port = 3000;
@@ -22,7 +23,7 @@ createConnection('mongodb+srv://koushik:koushik@cluster0-br4jq.mongodb.net/test?
 // });
 
 const t = new Teacher();
-t.name = 'Koushik';
+t.name = 'Koushik 2 ';
 
 const repo = getRepository(Teacher);
 repo.save([t]);
